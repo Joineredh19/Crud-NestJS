cc70d2799ff120516b4e76ccdcbb784a8e9d32cf
Joiner2003 <joineredh@gmail.com>
2024-09-18 22:52:52 -0500

    se agregan nuevas congiguraciones para vercel

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Column, PrimaryGeneratedColumn } from "typeorm";
+
+export class User {
+
+    @PrimaryGeneratedColumn()
+    @ApiProperty()
+    id: number;
+  
+    @Column()
+    @ApiProperty()
+    name: string;
+  
+    @Column()
+    @ApiProperty()
+    email: string;
+  
+
+}
